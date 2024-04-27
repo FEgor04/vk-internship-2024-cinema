b4c6d6719b0b783ec2903908397205a9d763ba64
Egor Fedorov <fegor2004@gmail.com>
2024-04-27 13:43:16 +0300

    add movie entity, add model

---
+++ src/entities/movie/model/movie.ts
@@ -0,0 +1,24 @@
+type MovieStatus = "filming" | "pre-production" | "completed" | "announced" | "post-production"
+
+export type Movie = {
+  id: number,
+  name: string,
+  enName: string,
+  description: string,
+  shortDescription: string,
+  genres: Array<string>,
+  countries: Array<string>
+  year: number,
+  slogan: string,
+  status: MovieStatus,
+  rating: {
+    kp: number,
+    imdb: number,
+    tmdb: number,
+    filmCritics: number,
+    russianFilmCritics: number,
+    await: number,
+  }
+  movieLength: number,
+  ageRating: number,
+}
