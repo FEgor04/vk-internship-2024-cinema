759230b92c6b7c5dc7b1a2ac06c372717ae16533
Egor Fedorov <fegor2004@gmail.com>
2024-04-27 13:09:11 +0300

    Initial commit

---
+++ src/app/routes/__root.tsx
@@ -0,0 +1,16 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+type RouterContext = {
+  queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
