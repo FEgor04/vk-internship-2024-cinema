677f7d2f8fa5bc17eb3db991eef92d7bc3202a59
Egor Fedorov <fegor2004@gmail.com>
2024-04-28 12:58:02 +0300

    feat: handle bad id error

--- src/app/routes/movies.$id.tsx
+++ src/app/routes/movies.$id.tsx
@@ -1,8 +1,26 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { MoviePage } from "@/pages/movie";
 
+class IdIsNanError extends Error {}
+
 export const Route = createFileRoute("/movies/$id")({
+  beforeLoad: ({ params }) => {
+    const id = parseInt(params.id);
+    if (isNaN(id)) {
+      // refactor: create exception for that
+      throw new IdIsNanError();
+    }
+  },
   component: PageComponent,
+  errorComponent: (error) => {
+    if (error.error instanceof IdIsNanError) {
+      return (
+        <div className="container mx-auto mt-8 text-center text-2xl font-bold text-destructive">
+          <h1>Ошибка! ID фильма должен быть числом!</h1>
+        </div>
+      );
+    }
+  },
 });
 
 function PageComponent() {
