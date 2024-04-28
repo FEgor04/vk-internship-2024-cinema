9008cdb7b441a7157b9084b8ef669b0778bbd383
Egor Fedorov <fegor2004@gmail.com>
2024-04-28 20:56:02 +0300

    add link to movies, add cv

--- src/pages/index/ui/index.tsx
+++ src/pages/index/ui/index.tsx
@@ -1,6 +1,7 @@
 import { FileIcon, Film } from "lucide-react";
 import { Button } from "@/shared/ui/button";
 import { Card } from "@/shared/ui/card";
+import { Link } from "@tanstack/react-router";
 
 export function IndexPage() {
   return (
@@ -12,12 +13,16 @@ export function IndexPage() {
               Возьмите меня на стажку
             </h1>
             <p className="flex max-w-[600px] flex-row space-x-4 text-lg md:text-xl">
-              <Button className="text-lg font-bold" size="lg">
-                <Film className="mr-2 size-5" />К фильмам
+              <Button className="text-lg font-bold" size="lg" asChild>
+                <Link to="/movies">
+                  <Film className="mr-2 size-5" />К фильмам
+                </Link>
               </Button>
-              <Button variant="ghost">
-                <FileIcon className="mr-2 size-4" />
-                Мое резюме
+              <Button variant="ghost" asChild>
+                <a href="/cv.pdf">
+                  <FileIcon className="mr-2 size-4" />
+                  Мое резюме
+                </a>
               </Button>
             </p>
           </div>
