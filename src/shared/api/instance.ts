98673c226e67cf9e81cb05dfe07a53c75506a264
Egor Fedorov <fegor2004@gmail.com>
2024-04-28 21:16:02 +0300

    feat(api): use `query-string` to serialize params

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,10 +1,12 @@
 import Axios, { AxiosError, AxiosRequestConfig } from "axios";
+import qs from "query-string";
 
 const baseURL = import.meta.env.PROD ? "https://api.kinopoisk.dev" : "/";
 const apiKey = import.meta.env.VITE_KINOPOISK_API_KEY;
 export const AXIOS_INSTANCE = Axios.create({
   baseURL,
   headers: { "X-API-KEY": apiKey },
+  paramsSerializer: (data) => qs.stringify(data),
 }); // use your own URL here or environment variable
 
 export const customInstance = <T>(
