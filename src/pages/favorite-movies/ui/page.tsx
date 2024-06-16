1af71afce93b21338d2bfc24dafff4d9175631dc
Egor Fedorov <fegor2004@gmail.com>
2024-06-16 19:56:00 +0300

    feat: add movies list to /favorite page

--- src/pages/favorite-movies/ui/page.tsx
+++ src/pages/favorite-movies/ui/page.tsx
@@ -1,3 +1,48 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+import { FavoriteStar, useFavoriteMovies } from "@/features/favorite-movie";
+import {
+  MovieCard,
+  MovieCardFallback,
+  getMoviesByIdsQueryOptions,
+} from "@/entities/movie";
+
 export function FavoriteMoviesPage() {
-  return <div>Hello favorite-movies!</div>;
+  return (
+    <div className="container mx-auto mt-8 space-y-8">
+      <header>
+        <h3 className="text-2xl font-bold">Избранные фильмы</h3>
+      </header>
+      <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+        <React.Suspense
+          fallback={new Array(50).fill(0).map(() => (
+            <MovieCardFallback />
+          ))}
+        >
+          <List />
+        </React.Suspense>
+      </section>
+    </div>
+  );
+}
+
+function List() {
+  const [favorites] = useFavoriteMovies();
+  const { data } = useSuspenseQuery({
+    ...getMoviesByIdsQueryOptions(favorites, {
+      limit: favorites.length,
+      page: 1,
+    }),
+  });
+  return (
+    <>
+      {data.movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          favoriteStarSlot={<FavoriteStar movieId={movie.id} />}
+        />
+      ))}
+    </>
+  );
 }
