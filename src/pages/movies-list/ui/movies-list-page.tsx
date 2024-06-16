7dc13ecad6f07cdb1821286e2cea01ca12180ce3
Egor Fedorov <fegor2004@gmail.com>
2024-06-16 10:59:47 +0300

    feat: add pagination

--- src/pages/movies-list/ui/movies-list-page.tsx
+++ src/pages/movies-list/ui/movies-list-page.tsx
@@ -1,6 +1,14 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { Link } from "@tanstack/react-router";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+} from "lucide-react";
 import { MovieFilters, getMoviesQueryOptions } from "@/features/filter-movie";
 import { Movie, MovieCard } from "@/entities/movie";
+import { Button } from "@/shared/ui/button";
 
 type Props = {
   initialData: {
@@ -19,10 +27,78 @@ export function MoviesListPage({ page, filters, initialData }: Props) {
     initialData,
   });
   return (
-    <div className="container mx-auto mt-8 grid grid-cols-1 gap-8 space-y-4 md:grid-cols-2 lg:grid-cols-4">
-      {data.movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div className="container mx-auto mt-8 space-y-8">
+      <header></header>
+      <section className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+        {data.movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </section>
+      <div className="flex flex-row items-center justify-end space-x-16 text-sm">
+        <div className="font-medium">
+          Страница {data.page} из {data.pages}
+        </div>
+        <PaginationControls page={data.page} pages={data.pages} />
+      </div>
     </div>
+  );
+}
+
+function PaginationControls({ page, pages }: Record<string, number>) {
+  const canGoBack = page > 1;
+  const canGoForward = page < pages;
+  return (
+    <nav className="space-x-2">
+      <Button
+        size="icon"
+        variant="outline"
+        className="size-8 p-0"
+        disabled={!canGoBack}
+        asChild
+      >
+        <Link from="/movies" search={(prev) => ({ ...prev, page: 1 })}>
+          <ChevronsLeft className="size-4" />
+        </Link>
+      </Button>
+      <Button
+        size="icon"
+        variant="outline"
+        className="size-8 p-0"
+        disabled={!canGoBack}
+        asChild
+      >
+        <Link
+          from="/movies"
+          search={(prev) => ({ ...prev, page: prev.page - 1 })}
+        >
+          <ChevronLeft className="size-4" />
+        </Link>
+      </Button>
+      <Button
+        size="icon"
+        variant="outline"
+        className="size-8 p-0"
+        disabled={!canGoForward}
+        asChild
+      >
+        <Link
+          from="/movies"
+          search={(prev) => ({ ...prev, page: prev.page + 1 })}
+        >
+          <ChevronRight className="size-4" />
+        </Link>
+      </Button>
+      <Button
+        size="icon"
+        variant="outline"
+        className="size-8 p-0"
+        disabled={!canGoForward}
+        asChild
+      >
+        <Link from="/movies" search={(prev) => ({ ...prev, page: pages })}>
+          <ChevronsRight className="size-4" />
+        </Link>
+      </Button>
+    </nav>
   );
 }
