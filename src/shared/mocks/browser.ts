03c0c7a7d87e59d4e9d750ba7cad80b69d21ae5f
Egor Fedorov <fegor2004@gmail.com>
2024-06-16 23:09:55 +0300

    fix ts errors

--- src/shared/mocks/browser.ts
+++ src/shared/mocks/browser.ts
@@ -1,8 +1,3 @@
-import { setupWorker } from "msw/browser";
-import { getAPIKinopoiskDevMock } from "../api/index.msw";
-
-const worker = setupWorker(...getAPIKinopoiskDevMock());
-
 export async function start(): Promise<void> {
   return new Promise((res) => res());
 }
